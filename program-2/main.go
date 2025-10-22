daa989724fa1af4dcca793d9f718b9f514c44c1f
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-22 21:01:44 +0000

    added Hail Mary prayer

--- program-2/main.go
+++ program-2/main.go
@@ -2,7 +2,7 @@
  * Author: Mr Loosli
  * Version 1.0.0
  * Date 2025-10-22
- * This program displays my name.
+ * This program displays the Hale Mary Prayer.
 */
 
 package main
@@ -11,14 +11,17 @@ import "fmt"
 
 func main () {
     //string literal text
-    fmt.Println("alexander loosli")
+    fmt.Println("Hail Mary")
 
     //string literal text 
-    fmt.Println("alexander loosli")
-    fmt.Println("NBE3CR english")
-    fmt.Println("TCJ3C1-1 wood working")
-    fmt.Println("OCS3UV computer science")
-    fmt.Println("MBFF3CR math")
-		
+    fmt.Println("Hail, Mary, full of grace,")
+    fmt.Println("the Lord is with thee.")
+    fmt.Println("Blessed art thou amongst women")
+    fmt.Println("and blessed is the fruit of thy womb, Jesus.")
+    fmt.Println("Holy Mary, Mother of God,")
+		fmt.Println(" pray for us sinners,")
+    fmt.Println(" now and at the hour of our death.")
+    fmt.Println("Amen.")
+
 		fmt.Println("\nDone")
 }
