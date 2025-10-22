02c4bd47648077cc80daa7658daf79ce8030b38d
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-22 21:06:11 +0000

    added a change to file. adding Happy birthday song

--- program-3/main.go
+++ program-3/main.go
@@ -1,8 +1,23 @@
-/**
- * @author mr loosli
- * @version 1.0.0
- * @date 2025-10-22
- * @fileoverview This program will display my name.
- */
+/*
+ * Author: Mr Loosli
+ * Version 1.0.0
+ * Date 2025-10-22
+ * This program displays the original Happy birthday song.
+*/
 
-// string literal text
+package main
+
+import "fmt"
+
+func main () {
+    //string literal text
+    fmt.Println("happy birthday")
+
+    //string literal text 
+    fmt.Println("Happy birthday to you")
+    fmt.Println("Happy birthday to you")
+    fmt.Println("Happy birthday, dear Alex")
+    fmt.Println("Happy birthday to you.")
+    
+		fmt.Println("\nDone")
+}
